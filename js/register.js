7e497d883924464b2e741e38034c4a64efeec138
BeaCardozo <beatriz.cardozo@correo.unimet.edu.ve>
2023-05-20 02:12:42 -0400

    changes

---
+++ js/register.js
@@ -0,0 +1,19 @@
+const username = document.getElementById('name');
+const button = document.getElementById('button');
+
+
+const validateInfo = () =>{
+    if (!username.value){
+        alert("Por favor, ingresa tu nombre!");
+    } else {
+        const info = {
+            username: username.value
+        }
+        window.location.href = "./index.html"
+    }
+}
+
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+    validateInfo()
+});
--- js/script.js
+++ js/script.js
@@ -1,24 +1,34 @@
-const username = document.getElementById('name')
-const button = document.getElementById('button')
-
-button.addEventListener('click', (e) => {
-    e.preventDefault()
-    const info = {
-        username: username.value
-    }
-})
-
-
 class Game {
     constructor(){
         this.GameStart = false;
         this.cardOne = null;
         this.cardTwo = null;
 
+        this.availableCards = [1, 2, 3, 4, 5, 7, 8];
+        this.order = [];
+
+        this.cards = Array.from(document.querySelectorAll('.game-container figure'));
+
+        this.game();
+    }
+
+    game() {
+        this.setNewOrder();
+    }
+
+    setNewOrder(){
+        this.order = this.availableCards.concat(this.availableCards);
+        this.order.sort(() => math.random() - 0.5);
     }
 }
 
+
+
 document.addEventListener("DOMContentLoaded", () =>{
+
+    function SelectCard() {
+        this.classList.add("selected")
+    }
 
     const cards = document.querySelectorAll("figure");
     cards.forEach(card => {
